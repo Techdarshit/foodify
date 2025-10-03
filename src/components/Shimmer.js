d5c6494af9f72ce165965f47e4e9b86c494e5857
Techdarshit <darshitpunglia@gmail.com>
2025-10-03 11:11:27 +0530

    Add

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,22 +1,18 @@
 const Shimmer = () => {
-    return (
-        <div className="shimmer-con">
-           <div className="shimmer-card"></div>
-           <div className="shimmer-card"></div>
-           <div className="shimmer-card"></div>
-           <div className="shimmer-card"></div>
-           <div className="shimmer-card"></div>
-           <div className="shimmer-card"></div>
-           <div className="shimmer-card"></div>
-           <div className="shimmer-card"></div>
-           <div className="shimmer-card"></div>
-           <div className="shimmer-card"></div>
-           <div className="shimmer-card"></div>
-           <div className="shimmer-card"></div>
-           <div className="shimmer-card"></div>
-           <div className="shimmer-card"></div>
-           <div className="shimmer-card"></div>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-wrap gap-6 p-6">
+      {Array(12)
+        .fill("")
+        .map((_, i) => (
+          <div
+            key={i}
+            className="w-48 h-60 rounded-xl 
+                       bg-gradient-to-r from-gray-200 via-gray-100 to-gray-200
+                       bg-[length:200%_100%] animate-shimmer"
+          ></div>
+        ))}
+    </div>
+  );
+};
+
 export default Shimmer;
